2bf20464e9062d7dd4ffa01ff32a7f27bbf1e6f6
marblekang <kangbm930@gmail.com>
2024-03-23 13:41:27 +0900

    commit

--- chapter2/src/MyReact.js
+++ chapter2/src/MyReact.js
@@ -1,13 +1,38 @@
-import { createHooks } from "./hooks";
-import { render as updateElement } from "./render";
+import { createHooks } from "./hooks.js";
+import { render as updateElement, jsx } from "./render.js";
 
 function MyReact() {
   const _render = () => {};
-  function render($root, rootComponent) {}
 
-  const { useState, useMemo, resetContext: resetHookContext } = createHooks(_render);
+  function render($root, rootComponent) {
+    updateElement($root, rootComponent());
+  }
+
+  const {
+    useState,
+    useMemo,
+    resetContext: resetHookContext,
+  } = createHooks(_render);
 
   return { render, useState, useMemo };
 }
 
 export default MyReact();
+
+let fn = null;
+const App = () => {
+  const [size, setSize] = MyReact().useState(1);
+
+  fn = setSize;
+  return jsx(
+    "div",
+    null,
+    ...Array.from({ length: size }).map((_, key) =>
+      jsx("p", null, `${key + 1}번째 자식`)
+    )
+  );
+};
+
+const $root = document.createElement("div");
+MyReact().render($root, App);
+fn(1);
